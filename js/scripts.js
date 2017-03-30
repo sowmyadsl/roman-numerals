19960ff77cf45263dc1505b64611778c4e299da3
Sowmya Dinavahi and Sean Pierce <sowmya@>
2017-03-30 10:19:27 -0700

    Add logic to handle numbers between 1 digit and 4 digits. Alert roman numerals only corresponding to the first place

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,41 @@
-var firstPlace = ["-", "I", "II", "III","IV","V","VI","VII","VIII","IX","X"];
+var firstPlace = ["-", "I", "II", "III","IV","V","VI","VII","VIII","IX"];
+var secondPlace = ["-","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
+
+var romanOutput = [];
+
+var convert = function(input){
+  // split input
+  var splitInput = input.split("");
+  // reverse order
+  var reversedInput = splitInput.reverse();
 
 
+  if (reversedInput.length === 1) {
+    reversedInput.forEach(function(num) {
+      num = firstPlace[num];
+      romanOutput.push(num);
+    });
+  } else if (reversedInput.length === 2) {
+    reversedInput.forEach(function(num) {
+      num = firstPlace[num];
+      romanOutput.push(num);
+    });
+  } else if (reversedInput.length === 3) {
+    reversedInput.forEach(function(num) {
+      num = firstPlace[num];
+      romanOutput.push(num);
+    });
+  } else {
+    reversedInput.forEach(function(num) {
+      num = firstPlace[num];
+      romanOutput.push(num);
+    });
+  }
 
-
-
+  // join numerals from array to a string
+  var joinedRoman = romanOutput.reverse().join("");
+  alert(joinedRoman);
+}
 
 
 
@@ -12,7 +44,7 @@ var firstPlace = ["-", "I", "II", "III","IV","V","VI","VII","VIII","IX","X"];
 $(document).ready(function(){
 $(".form1").submit(function(event){
   event.preventDefault();
-  var userInput =parseInt($("#user-input").val());
-  alert(firstPlace[userInput]);
+  var userInput =$("#user-input").val();
+  convert(userInput);
 });
 });
