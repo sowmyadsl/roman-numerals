e631cbaabe84d6c10061bc785939beacdd48fcbe
Sowmya Dinavahi and Sean Pierce <sowmya@>
2017-03-30 09:43:32 -0700

    Add logic to handle digits 0-10 to convert into roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
+var firstPlace = ["-", "I", "II", "III","IV","V","VI","VII","VIII","IX","X"];
 
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+$(".form1").submit(function(event){
+  event.preventDefault();
+  var userInput =parseInt($("#user-input").val());
+  alert(firstPlace[userInput]);
+});
 });
